aaa0bc77602127dafbeb8501efbb8685d6f9ef15
Arend van Beelen jr <arendjr@gmail.com>
2012-03-07 16:34:36 +0100

    Fix statics plugin doing multiple runs.

--- features/statics.py
+++ features/statics.py
@@ -7,7 +7,6 @@ import os
 class Feature(ShermanFeature):
 
     timestamps = {}
-    staticMap = {}
 
     rebuildNeeded = False
 
@@ -20,16 +19,24 @@ class Feature(ShermanFeature):
 
         print "    Copying statics..."
 
+        module["__staticMap__"] = {}
+
         for static in module["__manifest__"]["statics"]:
             path = self.projectBuilder.resolveFile(static["path"], modulePath + "/statics")
             mtime = os.stat(path).st_mtime
             if path in self.timestamps and mtime == self.timestamps[path]:
                 continue
 
-            print "      %s..." % static["path"]
-
             self.rebuildNeeded = True
             self.timestamps[path] = mtime
+
+        if not self.rebuildNeeded:
+            return
+
+        for static in module["__manifest__"]["statics"]:
+            print "      %s..." % static["path"]
+
+            path = self.projectBuilder.resolveFile(static["path"], modulePath + "/statics")
 
             (directory, fileName) = os.path.split(path)
             (baseName, extension) = os.path.splitext(fileName)
@@ -42,16 +49,15 @@ class Feature(ShermanFeature):
                 with open(destPath, "w") as outFile:
                     outFile.write(content)
 
-            self.staticMap[path] = destFileName
+            module["__staticMap__"][static["path"]] = destFileName
 
     def isRebuildNeeded(self, locale, moduleName, modulePath):
         return self.rebuildNeeded
 
     def sourcesConcatenated(self, locale, moduleName, modulePath):
-        if len(self.staticMap) > 0:
-            module = self.currentBuild.files[locale][moduleName]
-
+        module = self.currentBuild.files[locale][moduleName]
+        if "__staticMap__" in module and len(module["__staticMap__"]) > 0:
             staticDefs = []
-            for path in self.staticMap:
-                staticDefs.append("\"%s\": \"%s\"" % (path, self.staticMap[path]))
+            for path in module["__staticMap__"]:
+                staticDefs.append("\"%s\": \"%s\"" % (path, module["__staticMap__"][path]))
             module["__concat__"] += "Modules.%s.statics = { %s };\n" % (moduleName, ", ".join(staticDefs))
