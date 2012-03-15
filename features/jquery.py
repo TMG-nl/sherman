088eb8a8435ae19d2327f4fef58cae31de75f9ba
Arend van Beelen jr <arendjr@gmail.com>
2012-03-15 12:17:19 +0100

    Make jQuery/Zepto.js additional features.

---
+++ features/jquery.py
@@ -0,0 +1,17 @@
+from shermanfeature import ShermanFeature
+
+
+class Feature(ShermanFeature):
+
+    def __init__(self, config):
+        ShermanFeature.__init__(self, config)
+
+        self.additionalBootResources.append({
+            "path": "/features/jquery/jquery.js",
+            "excludeFromNamespace": True,
+            "runJsLint": False
+        })
+
+    @ShermanPriority(0)
+    def manifestLoaded(self, moduleName, modulePath, manifest):
+        ShermanFeature.manifestLoaded(self, moduleName, modulePath, manifest)
---
+++ features/zepto.py
@@ -0,0 +1,17 @@
+from shermanfeature import ShermanFeature
+
+
+class Feature(ShermanFeature):
+
+    def __init__(self, config):
+        ShermanFeature.__init__(self, config)
+
+        self.additionalBootResources.append({
+            "path": "/features/zepto/zepto.js",
+            "excludeFromNamespace": True,
+            "runJsLint": False
+        })
+
+    @ShermanPriority(0)
+    def manifestLoaded(self, moduleName, modulePath, manifest):
+        ShermanFeature.manifestLoaded(self, moduleName, modulePath, manifest)
